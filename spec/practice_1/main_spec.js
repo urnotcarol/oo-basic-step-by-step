e06423340314cc07cd874ddbef548f91e9d41ddd
Chrissie <urnotchrissie@outlook.com>
2015-08-28 10:03:27 +0800

    finish Practice_1

--- spec/practice_1/main_spec.js
+++ spec/practice_1/main_spec.js
@@ -1,4 +1,5 @@
 "use strict";
+var Person = require('../../src/practice_1.js');
 var _ = require("lodash");
 var chai = require("chai");
 var sinon = require("sinon");
@@ -6,14 +7,14 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
-describe("OO入门", function(){
-    it("写一个类-1", function(){
-        sinon.spy(console, 'log');
-
-        require("../../src/practice_1")
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '下面是部分学生的成绩详情\n沙龙逸\t语文124 数学148 英语137\n刘钰婷\t语文115 数学139 英语135\n韩林霖\t语文127 数学139 英语142\n平均成绩\t语文122 数学142 英语138';
-        
-        expect(expect_string).to.equal(result);
-    });
+describe("Practice 1", function(){
+    var thePerson = new Person("Jack", 17);
+    it("Person Constructor", function() {
+        expect(thePerson.name).to.exist;
+        expect(thePerson.age).to.exist;
+    })
+    it("Person introduce", function() {
+        var result = thePerson.introduce();
+        expect(result).to.equal("My name is Jack. I am 17 years old.")
+    })
 });
